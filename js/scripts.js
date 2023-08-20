2391e5c4409dc201976d9a511d324ebe7a76c2c2
dekoa00 <36924413+dekoa00@users.noreply.github.com>
2023-08-20 17:14:56 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -236,7 +236,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 10.786839223990599, lng: 106.6849420709829};
+    var location = {lat: 10.786800, lng: 106.684920};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -250,7 +250,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 10.786800, lng: 106.684920};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
